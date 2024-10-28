c7014a0d5e12617538cf550d5caa3033554d17b4
Orbit527 <peterschneider15@gmx.net>
2024-10-28 15:59:46 +0200

    redesigned RouteDetail Page by putting title in Header and adding margin under Map

--- Screens/RouteDetail.js
+++ Screens/RouteDetail.js
@@ -1,7 +1,7 @@
 import { ref, remove } from "firebase/database";
 import { View } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
-import { Appbar, Button, Text } from "react-native-paper";
+import { Appbar, Button, Divider, Text } from "react-native-paper";
 import RouteParametersCards from "../Components/RouteParametersCards.js";
 import { database } from "../firebaseConfig.js";
 import { styles } from "../StyleSheet.js";
@@ -9,7 +9,6 @@ import RouteMap from "../Components/RouteMap.js";
 
 export default function RouteDetail({ navigation, route }) {
   const { data } = route.params;
-  console.log(data);
 
   const deleteEntry = async (key) => {
     console.log("Deleted: " + key);
@@ -22,40 +21,16 @@ export default function RouteDetail({ navigation, route }) {
     <View style={{ flex: 1 }}>
       <Appbar.Header elevated mode="small">
         <Appbar.BackAction onPress={() => navigation.navigate("Route")} />
-        <Appbar.Content title="Route Details" titleStyle={{ fontSize: 24 }} />
+        <Appbar.Content title={data.title} titleStyle={{ fontSize: 24 }} />
       </Appbar.Header>
 
       <View style={styles.container}>
-        <Text variant="titleLarge">{data.title}</Text>
-
         <RouteMap
           locationLat={data.coordinates[0].latitude}
           locationLong={data.coordinates[0].longitude}
           coordinates={data.coordinates}
           tracking={false}
         />
-
-        {/*
-        <MapView
-          style={{ width: "100%", height: "50%" }}
-          initialRegion={{
-            latitude: data.coordinates ? data.coordinates[0].latitude : 0,
-            longitude: data.coordinates ? data.coordinates[0].longitude : 0,
-            latitudeDelta: 0.002,
-            longitudeDelta: 0.002,
-          }}
-        >
-          {data.coordinates ? (
-            <Polyline
-              coordinates={data.coordinates}
-              strokeColor="#0000AF"
-              strokeWidth={1}
-            />
-          ) : (
-            <Text>No data</Text>
-          )}
-        </MapView>
-        */}
 
         <RouteParametersCards
           duration={data.duration}
@@ -65,14 +40,17 @@ export default function RouteDetail({ navigation, route }) {
           endTime={data.endTime}
         />
 
-        <Button
-          mode="contained"
-          icon="trash-can-outline"
-          buttonColor="darkred"
-          onPress={() => deleteEntry(data.key)}
-        >
-          Delete
-        </Button>
+        <View style={{ alignItems: "center" }}>
+          <Button
+            mode="contained"
+            icon="trash-can-outline"
+            buttonColor="darkred"
+            style={styles.button}
+            onPress={() => deleteEntry(data.key)}
+          >
+            Delete
+          </Button>
+        </View>
       </View>
     </View>
   );
