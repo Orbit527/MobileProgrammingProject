3ae6c2d28a54ae80de14b03fe31c9129aca1fdb0
Orbit527 <peterschneider15@gmx.net>
2024-10-31 12:11:11 +0200

    added basic login/logout and registration function in Profile page with Firebase Authentication

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,8 +1,69 @@
-import {  View } from "react-native";
-import { Appbar, Text } from "react-native-paper";
-import { styles } from "../StyleSheet.js"
+import {
+  equalTo,
+  onValue,
+  orderByChild,
+  push,
+  query,
+  ref,
+} from "firebase/database";
+import { useEffect, useState } from "react";
+import { KeyboardAvoidingView, View } from "react-native";
+import { Appbar, Button, Text, TextInput } from "react-native-paper";
+import { database, firebaseAuth } from "../firebaseConfig.js";
+import { styles } from "../StyleSheet.js";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  User,
+} from "firebase/auth";
 
 export default function Profile() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = firebaseAuth;
+
+  const [user, setUser] = useState(null);
+
+  const signIn = async () => {
+    console.log("signIn");
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+
+  const signUp = async () => {
+    console.log("signUp");
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(response);
+      alert("Check your email!");
+    } catch (error) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+
+  const signOut = () => {
+    firebaseAuth.signOut();
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      console.log("user:" + user);
+      setUser(user);
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <Appbar.Header elevated mode="small">
@@ -10,7 +71,33 @@ export default function Profile() {
       </Appbar.Header>
 
       <View style={styles.container}>
-        <Text variant="titleLarge">Profile</Text>
+        <KeyboardAvoidingView behavior="padding">
+          <TextInput
+            style={{ marginBottom: 15 }}
+            placeholder="Email"
+            value={email}
+            mode="outlined"
+            onChangeText={(text) => setEmail(text)}
+          />
+          <TextInput
+            style={{ marginBottom: 15 }}
+            placeholder="Password"
+            value={password}
+            mode="outlined"
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Button mode="contained" icon="login" onPress={() => signIn()}>
+            Login
+          </Button>
+          <Button mode="contained" icon="login" onPress={() => signUp()}>
+            Create Account
+          </Button>
+          <Button mode="contained" icon="login" onPress={() => signOut()}>
+            Sign Out
+          </Button>
+
+          {user ? <Text>Logged in with: {user.email}</Text> : <Text>Not logged in</Text>}
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
