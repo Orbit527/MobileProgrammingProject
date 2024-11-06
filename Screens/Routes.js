4026c60f970beb5da141a0457fcfebf37095167f
Orbit527 <peterschneider15@gmx.net>
2024-11-06 19:31:01 +0200

    added route loading from Firebase based on UID of logged in user

--- Screens/Routes.js
+++ Screens/Routes.js
@@ -1,9 +1,16 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { onValue, ref } from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
-import { ActivityIndicator, Appbar, Button, Card, PaperProvider, Text } from "react-native-paper";
+import {
+  ActivityIndicator,
+  Appbar,
+  Button,
+  Card,
+  Text,
+} from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { database } from "../firebaseConfig.js";
+import { database, firebaseAuth } from "../firebaseConfig.js";
 import { distanceToKm, formatDuration } from "../HelperClass.js";
 import { styles } from "../StyleSheet.js";
 
@@ -13,43 +20,56 @@ export default function Routes({ navigation }) {
   const [overallDistance, setOverallDistance] = useState(0);
   const [overallDuration, setOverallDuration] = useState(0);
 
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
-    const routesRef = ref(database, "/routes");
-    onValue(routesRef, (snapshot) => {
-      data = snapshot.val();
-
-      if (!data) {
-        setRoutes([]);
-        setOverallDistance(0);
-        setOverallDuration(0);
-        return;
-      }
-
-      const dataWithKey = Object.entries(data).map(([key, other]) => ({
-        key,
-        ...other,
-      }));
-
-      setRoutes(dataWithKey);
-      setLoading(true);
-
-      let totalDistance = Object.values(dataWithKey).reduce(
-        (acc, { distance }) => {
-          return acc + parseFloat(distance);
-        },
-        0
-      );
-      setOverallDistance(totalDistance);
-
-      let totalDuration = Object.values(dataWithKey).reduce(
-        (acc, { duration }) => {
-          return acc + parseFloat(duration);
-        },
-        0
-      );
-      setOverallDuration(totalDuration);
+    onAuthStateChanged(firebaseAuth, (user) => {
+      console.log("user:" + user);
+      setUser(user);
     });
   }, []);
+
+  useEffect(() => {
+    // check if user is logged in
+    if (user) {
+      const routesRef = ref(database, "/users/" + user.uid);
+      onValue(routesRef, (snapshot) => {
+        data = snapshot.val();
+
+        // if there is no data, then display nothing, set states to 0 and return
+        if (!data) {
+          setRoutes([]);
+          setOverallDistance(0);
+          setOverallDuration(0);
+          return;
+        }
+
+        const dataWithKey = Object.entries(data).map(([key, other]) => ({
+          key,
+          ...other,
+        }));
+
+        setRoutes(dataWithKey);
+        setLoading(true);
+
+        let totalDistance = Object.values(dataWithKey).reduce(
+          (acc, { distance }) => {
+            return acc + parseFloat(distance);
+          },
+          0
+        );
+        setOverallDistance(totalDistance);
+
+        let totalDuration = Object.values(dataWithKey).reduce(
+          (acc, { duration }) => {
+            return acc + parseFloat(duration);
+          },
+          0
+        );
+        setOverallDuration(totalDuration);
+      });
+    }
+  }, [user]);
 
   const showDetails = (key) => {
     const route = routes.find((route) => route.key === key);
@@ -91,46 +111,50 @@ export default function Routes({ navigation }) {
           Your Routes
         </Text>
 
-        {loading ? (
-          <FlatList
-            ListEmptyComponent={
-              <Text variant="titleMedium">No Routes yet...</Text>
-            }
-            data={routes}
-            renderItem={({ item }) => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                  alignItems: "left",
-                  margin: 4,
-                }}
-              >
-                <Card mode="elevated" style={styles.cardLong}>
-                  <Card.Content style={styles.cardContent}>
-                    <Text variant="titleMedium">{item.title}</Text>
-                  </Card.Content>
+        {user ? (
+          loading ? (
+            <FlatList
+              ListEmptyComponent={
+                <Text variant="titleMedium">No Routes yet...</Text>
+              }
+              data={routes}
+              renderItem={({ item }) => (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    alignItems: "left",
+                    margin: 4,
+                  }}
+                >
+                  <Card mode="elevated" style={styles.cardLong}>
+                    <Card.Content style={styles.cardContent}>
+                      <Text variant="titleMedium">{item.title}</Text>
+                    </Card.Content>
 
-                  <Card.Content style={styles.cardContent}>
-                    <Icon name="map-marker-distance" size={24} color="#000" />
-                    <Text>{distanceToKm(item.distance)} km</Text>
-                    <Icon name="clock-outline" size={24} color="#000" />
-                    <Text>{formatDuration(item.duration)} h</Text>
-                    <Button
-                      onPress={() => showDetails(item.key)}
-                      size={32}
-                      icon="arrow-right"
-                      contentStyle={{ flexDirection: "row-reverse" }}
-                    >
-                      Details
-                    </Button>
-                  </Card.Content>
-                </Card>
-              </View>
-            )}
-          />
+                    <Card.Content style={styles.cardContent}>
+                      <Icon name="map-marker-distance" size={24} color="#000" />
+                      <Text>{distanceToKm(item.distance)} km</Text>
+                      <Icon name="clock-outline" size={24} color="#000" />
+                      <Text>{formatDuration(item.duration)} h</Text>
+                      <Button
+                        onPress={() => showDetails(item.key)}
+                        size={32}
+                        icon="arrow-right"
+                        contentStyle={{ flexDirection: "row-reverse" }}
+                      >
+                        Details
+                      </Button>
+                    </Card.Content>
+                  </Card>
+                </View>
+              )}
+            />
+          ) : (
+            <ActivityIndicator animating={true} />
+          )
         ) : (
-          <ActivityIndicator animating={true} />
+          <Text>You are not logged in!</Text>
         )}
       </View>
     </View>
