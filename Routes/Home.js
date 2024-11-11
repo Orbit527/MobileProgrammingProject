4139dce4a737b97e0118d63ca920195973b5979c
Orbit527 <peterschneider15@gmx.net>
2024-11-11 20:55:59 +0200

    added Background Image and Text to Home

--- Routes/Home.js
+++ Routes/Home.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { ImageBackground, ScrollView, View } from "react-native";
 import { Appbar, Text } from "react-native-paper";
 import { styles } from "../Styles/StyleSheet.js";
 import WeatherCards from "../Components/WeatherCards.js";
@@ -10,9 +10,26 @@ export default function Home() {
         <Appbar.Content title="Home" titleStyle={{ fontSize: 24 }} />
       </Appbar.Header>
 
-      <View style={styles.container}>
-        <Text variant="titleLarge">Home</Text>
-        <WeatherCards lat={"60.1695"} long={"24.9354"}/>
+      <View style={{ flex: 1 }}>
+      <ScrollView>
+        <ImageBackground
+          source={{
+            uri: "https://images.pexels.com/photos/1390403/pexels-photo-1390403.jpeg",
+          }}
+          resizeMode="cover"
+          style={{ flex: 1 }}
+        >
+          <Text variant="displayLarge" style={{ margin: 20, color: "white" }}>
+            Welcome to Run for Fun
+          </Text>
+        </ImageBackground>
+        <View style={styles.container}>
+          <Text variant="headlineMedium" style={{ marginVertical: 20}}>
+            Current Weather Conditions
+          </Text>
+          <WeatherCards lat={"60.1695"} long={"24.9354"} />
+        </View>
+        </ScrollView>
       </View>
     </View>
   );
