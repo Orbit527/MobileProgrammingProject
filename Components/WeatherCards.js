6d08a565fb844d6b53701059a227a1654ea7a3ce
Orbit527 <peterschneider15@gmx.net>
2024-11-15 10:47:07 +0200

    fixed WeatherCards parameters from fetch, to display right forecast tempreature both in hourly and daily

--- Components/WeatherCards.js
+++ Components/WeatherCards.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 
 export default function WeatherCards({ lat, long }) {
   const [response, setResponse] = useState(null);
+  const [currentHour, setCurrentHour] = useState(0);
 
   //Docu: https://open-meteo.com/en/docs#latitude=60.1695&longitude=24.9354&hourly=temperature_2m&daily=weather_code
 
@@ -27,6 +28,7 @@ export default function WeatherCards({ lat, long }) {
 
   useEffect(() => {
     handleFetch();
+    setCurrentHour(new Date().getHours());
   }, [lat, long]);
 
   useEffect(() => {
@@ -81,10 +83,10 @@ export default function WeatherCards({ lat, long }) {
               <Text variant="titleMedium">+1 h</Text>
               <Divider style={{ height: 1, width: "160%" }} />
               <Text variant="titleSmall">
-                {response && response.hourly.temperature_2m[0]} °C
+                {response && response.hourly.temperature_2m[currentHour]} °C
               </Text>
               <Text variant="titleSmall">
-                {response && response.hourly.precipitation_probability[0]} %
+                {response && response.hourly.precipitation_probability[currentHour]} %
               </Text>
             </Card.Content>
           </Card>
@@ -94,10 +96,10 @@ export default function WeatherCards({ lat, long }) {
               <Text variant="titleMedium">+2 h</Text>
               <Divider style={{ height: 1, width: "160%" }} />
               <Text variant="titleSmall">
-                {response && response.hourly.temperature_2m[1]} °C
+                {response && response.hourly.temperature_2m[currentHour + 1]} °C
               </Text>
               <Text variant="titleSmall">
-                {response && response.hourly.precipitation_probability[1]} %
+                {response && response.hourly.precipitation_probability[currentHour + 1]} %
               </Text>
             </Card.Content>
           </Card>
@@ -106,29 +108,6 @@ export default function WeatherCards({ lat, long }) {
             <Card.Content style={styles.cardContentTall}>
               <Text variant="titleMedium">+1 d</Text>
               <Divider style={{ height: 1, width: "160%" }} />
-              <Text variant="titleSmall">
-                {
-                  //round to 1 decimal place
-                  response &&
-                    Math.round(
-                      ((response.daily.temperature_2m_min[0] +
-                        response.daily.temperature_2m_max[0]) /
-                        2) *
-                        10
-                    ) / 10
-                }{" "}
-                °C
-              </Text>
-              <Text variant="titleSmall">
-                {response && response.daily.precipitation_probability_mean[0]} %
-              </Text>
-            </Card.Content>
-          </Card>
-
-          <Card mode="contained" style={styles.card}>
-            <Card.Content style={styles.cardContentTall}>
-              <Text variant="titleMedium">+2 d</Text>
-              <Divider style={{ height: 1, width: "150%" }} />
               <Text variant="titleSmall">
                 {
                   //round to 1 decimal place
@@ -144,6 +123,29 @@ export default function WeatherCards({ lat, long }) {
               </Text>
               <Text variant="titleSmall">
                 {response && response.daily.precipitation_probability_mean[1]} %
+              </Text>
+            </Card.Content>
+          </Card>
+
+          <Card mode="contained" style={styles.card}>
+            <Card.Content style={styles.cardContentTall}>
+              <Text variant="titleMedium">+2 d</Text>
+              <Divider style={{ height: 1, width: "150%" }} />
+              <Text variant="titleSmall">
+                {
+                  //round to 1 decimal place
+                  response &&
+                    Math.round(
+                      ((response.daily.temperature_2m_min[2] +
+                        response.daily.temperature_2m_max[2]) /
+                        2) *
+                        10
+                    ) / 10
+                }{" "}
+                °C
+              </Text>
+              <Text variant="titleSmall">
+                {response && response.daily.precipitation_probability_mean[2]} %
               </Text>
             </Card.Content>
           </Card>
